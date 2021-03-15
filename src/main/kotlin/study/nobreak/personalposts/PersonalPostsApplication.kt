c00d15df13b315566b8d88a3b7d72b05c44983f7
HeeChul So <thrrkthrrk@gmail.com>
2021-03-15 22:28:32 +0900

    Initial project by spring initializr

---
+++ src/main/kotlin/study/nobreak/personalposts/PersonalPostsApplication.kt
@@ -0,0 +1,11 @@
+package study.nobreak.personalposts
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PersonalPostsApplication
+
+fun main(args: Array<String>) {
+    runApplication<PersonalPostsApplication>(*args)
+}
---
+++ src/test/kotlin/study/nobreak/personalposts/PersonalPostsApplicationTests.kt
@@ -0,0 +1,13 @@
+package study.nobreak.personalposts
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PersonalPostsApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
