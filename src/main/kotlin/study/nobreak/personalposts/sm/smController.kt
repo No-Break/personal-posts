ac2322da428e1117146e41122a42e05d7de29a52
seongmin <qowlxj08@naver.com>
2021-03-24 21:29:33 +0900

    sm hello word test!

--- src/main/kotlin/study/nobreak/personalposts/sm/smController.kt
+++ src/main/kotlin/study/nobreak/personalposts/sm/smController.kt
@@ -1,4 +1,11 @@
 package study.nobreak.personalposts.sm
 
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
 class smController {
+
+    @GetMapping
+    fun hello(): String = "hello world"
 }
