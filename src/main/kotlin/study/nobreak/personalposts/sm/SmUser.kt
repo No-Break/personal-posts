41395da865c20e23ddea8f26c2e0a37de21d5e44
OseongminO <25198726+OseongminO@users.noreply.github.com>
2021-03-29 10:16:28 +0900

    Feature/user (#3)
    * sm hello word test
    
    * sm hello word test!
    
    * Fixed application.yaml in ddl-auto:create > update
    
    * update create user & check user
    
    * Correct class name,Constructor...

---
+++ src/main/kotlin/study/nobreak/personalposts/sm/SmUser.kt
@@ -0,0 +1,11 @@
+package study.nobreak.personalposts.sm
+
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.Id
+
+@Entity
+class SmUser(
+    var userId:String,
+    var password:String,
+    @Id @GeneratedValue var id:Long?=null)
