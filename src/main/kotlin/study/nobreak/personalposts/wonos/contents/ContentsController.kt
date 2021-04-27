55d7facbe0c386bb9c9236d4667e961e57494d1e
theimc-wonho <jsa1031109@naver.com>
2021-04-27 17:51:49 +0900

    Add create contente feature
    add ResponseStatus annotation in getAllContents method and add createContent using JPARepository

--- src/main/kotlin/study/nobreak/personalposts/wonos/contents/ContentsController.kt
+++ src/main/kotlin/study/nobreak/personalposts/wonos/contents/ContentsController.kt
@@ -1,6 +1,7 @@
 package study.nobreak.personalposts.wonos.contents
 
-
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.GetMapping
@@ -12,8 +13,15 @@ import study.nobreak.personalposts.wonos.contents.vo.ContentsVo
 class ContentsController(
     private val contentService: ContentsService
 ) {
+    @ResponseStatus(HttpStatus.OK)
     @GetMapping("/contents")
     fun getAllContents(contentsVo: ContentsVo){
         contentService.searchAllContents(contentsVo.title, contentsVo.mainText, contentsVo.writer)
+    }
+
+    @PostMapping("/contents/create")
+    @ResponseStatus(HttpStatus.CREATED)
+    fun createContent(contentsVo: ContentsVo){
+        contentService.createContent(contentsVo.title, contentsVo.mainText, contentsVo.writer)
     }
 }
