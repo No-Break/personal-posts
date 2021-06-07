e85e9a06d6bf0488387736ab55dc2773f7af982b
Heechul So <thrrkthrrk@gmail.com>
2021-06-08 08:12:52 +0900

    Fix main test case to avoid context loading

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,7 +25,10 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     runtimeOnly("com.h2database:h2")
     runtimeOnly("mysql:mysql-connector-java")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.boot:spring-boot-starter-test") {
+        exclude(module = "mockito-core")
+    }
+    testImplementation("com.ninja-squad:springmockk:3.0.1")
 }
 
 jacoco {
--- src/test/kotlin/study/nobreak/personalposts/PersonalPostsApplicationTests.kt
+++ src/test/kotlin/study/nobreak/personalposts/PersonalPostsApplicationTests.kt
@@ -1,13 +1,28 @@
 package study.nobreak.personalposts
 
+import io.mockk.every
+import io.mockk.mockkStatic
+import io.mockk.unmockkStatic
+import io.mockk.verify
 import org.junit.jupiter.api.Test
-import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.test.context.ActiveProfiles
+import org.springframework.boot.ApplicationContextFactory
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.WebApplicationType
 
-@SpringBootTest
-@ActiveProfiles("test")
-class PersonalPostsApplicationTests {
+internal class PersonalPostsApplicationTests {
     @Test
-    fun contextLoads() {
+    fun mainShouldStartMyApplication() {
+        mockkStatic(SpringApplication::class)
+        
+        every {
+            SpringApplication.run(any())
+        } returns ApplicationContextFactory.DEFAULT.create(WebApplicationType.NONE)
+        
+        main(arrayOf())
+        
+        verify {
+            SpringApplication.run(PersonalPostsApplication::class.java)
+        }
+        unmockkStatic(SpringApplication::class)
     }
 }
