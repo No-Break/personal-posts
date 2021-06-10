c218ef1ac3cd13e85b16e36ec2d226b1d794b541
SeongMin Kim <25198726+OseongminO@users.noreply.github.com>
2021-06-10 21:03:39 +0900

    fixed dir (#19)
    머지하겠습니다.

--- src/main/kotlin/study/nobreak/personalposts/sm/service/SmBoardService.kt
+++ src/main/kotlin/study/nobreak/personalposts/sm/service/SmBoardService.kt
@@ -1,6 +1,7 @@
-package study.nobreak.personalposts.sm
+package study.nobreak.personalposts.sm.service
 
 import org.springframework.stereotype.Component
+import study.nobreak.personalposts.sm.SmBoard
 
 @Component
 public interface SmBoardService {
